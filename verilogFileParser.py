623b6df280d6be345308424a97a79e14f7d788d7
Manjari Gupta <manjarigupta28.04@gmail.com>
2021-08-19 23:45:38 +0530

    Improvements: file arg from command line, result written to a file, multi-bit parametrs also identified

--- verilogFileParser.py
+++ verilogFileParser.py
@@ -1,72 +1,85 @@
 # Gives a list of input pins, output pins and parameters for given design
 
-import re
+def verilogFileParser (verilogFile):
+    import re
 
-moduleRegex = re.compile(r'(module )(.*)( (#)?\()')
+    moduleRegex = re.compile(r'(module )(.*)( (#)?\()')
 
-portRegex = re.compile(r'''(
-((in|out)put)       #port declaration
-(\s)+
-(\w{3,4})*               #wire/reg
-(\s)*
-(\[[A-Z-]*(\d)* : [A-Z-]*(\d)*])?  #if vector
-(\s)*
-(\w+)               #name
-(,|;)?
-                #seperator
-)''', re.VERBOSE)
+    portRegex = re.compile(r'''(
+    ((in|out)put)       #port declaration
+    (\s)+
+    (\w{3,4})*               #wire/reg
+    (\s)*
+    (\[[A-Z-]*(\d)* : [A-Z-]*(\d)*])?  #if vector
+    (\s)*
+    (\w+)               #name
+    (,|;)?             #seperator
+    )''', re.VERBOSE)
+    
+    #CHANGE: think about what to do for declarations like input clk, reset..
 
-#CHANGE: think about what to do for declarations like input clk, reset..
+    parameterRegex = re.compile(r'''(
+    (parameter)         #declaration
+    (\s)+
+    [A-Z_0-9]+               #name
+    (\s)*
+    =    
+    (\s)*               
+    ([A-Za-z0-9']+)               #value
+    (,|\;)+               #end  
+    )''', re.VERBOSE)
 
-parameterRegex = re.compile(r'''(
-(parameter)         #declaration
-(\s)+
-[A-Z]+               #name
-(\s)*
-=    
-(\s)*               
-(\w+)               #value
-(,|\;)+               #end  
-)''', re.VERBOSE)
+    #access Verilog file
+    fileObject = open(verilogFile)
+    text = fileObject.read()
 
-#CHANGE: parameters with vector spec
-#CHANGE:take file name as arguement from terminal
-#access verilog file
-fileObject = open(r'MUX.v')
-text = fileObject.read()
+    #Find Module name
+    mo = moduleRegex.search(text)
+    moduleName = mo.group(2)
+    
+    #open a file to write to
+    fileName = 'LOG{name}.txt'.format(name = moduleName)
+    writeFileObject = open(fileName, 'w')
+    writeFileObject.write("The design is of a:\n\t%s\n" %moduleName)
+    
+    #ports
+    po = portRegex.findall(text)
+    ports = []
+    for i in po:
+        ports.append(i[0])
+        inputs = []
+        outputs = []
+    for i in ports:
+        name = str(i)
+        if name[0:5] == 'input':
+            inputs.append(name)
+        else:
+            outputs.append(i)
+    writeFileObject.write("List of input ports:\n\t%s\n" %inputs)
+    writeFileObject.write("List of output ports:\n\t%s\n" %outputs)
 
-#Find Module name
-mo = moduleRegex.search(text)
-moduleName = mo.group(2)
-print("The design is of a:\n\t%s\n" %moduleName)
+    #CHANGE: format the ports how they are saved using groups
 
-#ports
-po = portRegex.findall(text)
-ports = []
-for i in po:
-    ports.append(i[0])
-inputs = []
-outputs = []
-for i in ports:
-    name = str(i)
-    if name[0:5] == 'input':
-        inputs.append(name)
-    else:
-        outputs.append(i)
-print("List of input ports:\n\t%s\n" %inputs)
-print("List of output ports:\n\t%s\n" %outputs)
-
-#CHANGE: format the ports how they are saved using groups
-
-#parameters
-lo = parameterRegex.findall(text)
-parameters = []
-for i in lo:
-    parameters.append(i[0])
-print("List of parameters:\n\t%s" %parameters)     
+    #parameters
+    lo = parameterRegex.findall(text)
+    parameters = []
+    for i in lo:
+        parameters.append(i[0])
+    writeFileObject.write("List of parameters:\n\t%s" %parameters)     
 
 
-#CHANGE: write to a new file
+
+import sys
+
+if len(sys.argv) > 2:
+    print("Invalid number of arguements")
+    sys.exit()
+
+else:
+    text = sys.argv[1]
+    verilogFileParser(text)
+    
+
 
 
                        
